3f10180a6ae542bc7276e2506c89befeb4a7f786
Steven Paalz <stevenpaalz@gmail.com>
2023-04-03 19:40:21 -0400

    create buy dropdown

--- frontend/src/components/Navigation/BuyDropDown.js
+++ frontend/src/components/Navigation/BuyDropDown.js
@@ -4,22 +4,29 @@ import { Link } from 'react-router-dom';
 function BuyDropDown() {
 
     return(
-        <div className='main-dropdown' id='buy-dropdown'>
-            <div className="dropdown-subnav">
-                <h3>Homes for sale in your area</h3>
+        <div className='main-dropdown'>
+            <div id='buy-dropdown' className="dropdown-subnav">
                 <div>
-                <ul className='col1'>
-                    <li><Link to="/">Homes for sale</Link></li>
-                    <li><Link to="/">Foreclosures</Link></li>
-                    <li><Link to="/">For sale by owner</Link></li>
-                    <li><Link to="/">Open houses</Link></li>
-                </ul>
-                <ul className='col1'>
-                    <li><Link to="/">New construction</Link></li>
-                    <li><Link to="/">Coming soon</Link></li>
-                    <li><Link to="/">Recent home sales</Link></li>
-                    <li><Link to="/">All homes</Link></li>
-                </ul>
+                    <h3>Homes for sale</h3>
+                    <ul id='buy-dropdown-col1' className='main-dropdown-col'>
+                        <li><Link to="/">Homes for sale</Link></li>
+                        <li><Link to="/">Foreclosures</Link></li>
+                        <li><Link to="/">For sale by owner</Link></li>
+                        <li><Link to="/">Open houses</Link></li>
+                        <li><Link to="/">New construction</Link></li>
+                        <li><Link to="/">Coming soon</Link></li>
+                        <li><Link to="/">Recent home sales</Link></li>
+                        <li><Link to="/">All homes</Link></li>
+                    </ul>
+                </div>
+                <div className='not-first-col-main-dropdown'>
+                    <h3>Resources</h3>
+                        <ul id='buy-dropdown-col2' className='main-dropdown-col'>
+                            <li><Link to="/">Buyers Guide</Link></li>
+                            <li><Link to="/">Foreclosure center</Link></li>
+                            <li><Link to="/">Real estate app</Link></li>
+                            <li><Link to="/">Down payment assistance</Link></li>
+                        </ul>
                 </div>
             </div>
         </div>
