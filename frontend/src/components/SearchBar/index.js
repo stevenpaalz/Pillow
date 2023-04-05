88c323eea803efddc68229dda68807981933b46e
Steven Paalz <stevenpaalz@gmail.com>
2023-04-05 12:26:21 -0400

    lunch time

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -8,19 +8,23 @@ function SearchBar() {
 
     return(
         <div id="index-page-search-bar">
-            <form onSubmit={handleSubmit} className="open-sans" id='index-search'>
-                <input placeholder='City, Neighborhood, ZIP, Address'></input>
-                <span><button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button></span>
-            </form>
-            {/* <select id='listings-page-sort-by'>
-                <option>Newest</option>
-                <option>Price (High to Low)</option>
-                <option>Price (Low to High)</option>
-                <option>Bedrooms</option>
-                <option>Bathrooms</option>
-                <option>Square Feet</option>
-            </select> */}
-
+            <div id="search-bar-form">
+                <form onSubmit={handleSubmit} className="open-sans" id='index-search'>
+                    <input placeholder='City, Neighborhood, ZIP, Address'></input>
+                    <span><button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button></span>
+                </form>
+                <button id='sale-type-sort' className='search-bar-sort'>
+                    <form>
+                        <label htmlFor="for-sale-radio">For Sale</label>
+                        <input id='for-sale-radio' type='radio' />
+                        <label htmlFor="for-rent-radio">Rent</label>
+                        <input id='for-rent-radio' type='radio' />
+                    </form>
+                </button>
+            </div>
+            <div id="saved-homes">
+                <button>Saved Homes</button>
+            </div> 
         </div>
     )
 
