a0c673e296c862111390faf813ebf6e258d81e74
Steven Paalz <stevenpaalz@gmail.com>
2023-04-05 22:41:16 -0400

    update show page styles

--- frontend/src/components/ListingShowPage/ShowDetailsFacts.js
+++ frontend/src/components/ListingShowPage/ShowDetailsFacts.js
@@ -5,35 +5,39 @@ function ShowDetailsFacts({listing}) {
     return(
         <div id="show-details-facts">
             <h3>Facts and features</h3>
-            <h4>Interior Details</h4>
-            <h5>Bedrooms and bathrooms</h5>
-            <ul>
-                <li>Bedrooms: {listing.numBeds}</li>
-                <li>Bathrooms: {listing.numBaths}</li>
-                <li>Full bathrooms: {Math.floor(listing.numBaths)}</li>
-            </ul>
-            <h5>Cooling</h5>
-            <ul>
-                <li>Cooling featutes: {listing.airCon}</li>
-            </ul>
-            <h4>Construction Details</h4>
-            <h5>Type and style</h5>
-            <ul>
-                <li>Home type: {listing.homeType}</li>
-            </ul>
-            <h5>Condition</h5>
-            <ul>
-                <li>New Construction: {listing.yearBuilt >= 2022 ? "Yes" : "No"}</li>
-                <li>Year built: {listing.yearBuilt}</li>
-            </ul>
-            <h4>Community and Neighborhood Details</h4>
-            <h5>Location</h5>
-            <ul>
-                <li>Region: {listing.city}</li>
-            </ul>
+            <div id='interior-details'>
+                <h4>Interior Details</h4>
+                <h5>Bedrooms and bathrooms</h5>
+                <ul>
+                    <li>Bedrooms: {listing.numBeds}</li>
+                    <li>Bathrooms: {listing.numBaths}</li>
+                    <li>Full bathrooms: {Math.floor(listing.numBaths)}</li>
+                </ul>
+                <h5>Cooling</h5>
+                <ul>
+                    <li>Cooling featutes: {listing.airCon}</li>
+                </ul>
+            </div>
+            <div id='construction-details'>
+                <h4>Construction Details</h4>
+                <h5>Type and style</h5>
+                <ul>
+                    <li>Home type: {listing.homeType}</li>
+                </ul>
+                <h5>Condition</h5>
+                <ul>
+                    <li>New Construction: {listing.yearBuilt >= 2022 ? "Yes" : "No"}</li>
+                    <li>Year built: {listing.yearBuilt}</li>
+                </ul>
+            </div>
+            <div id='community'>
+                <h4>Community and Neighborhood Details</h4>
+                <h5>Location</h5>
+                <ul>
+                    <li>Region: {listing.city}</li>
+                </ul>
+            </div>
         </div>
-        
-
     )
 }
 
--- frontend/src/components/ListingShowPage/ShowRightSubnav.js
+++ frontend/src/components/ListingShowPage/ShowRightSubnav.js
@@ -5,15 +5,23 @@ function ShowRightSubnav() {
     const clickOverview = (e) => {
         const overview = document.querySelector('#show-right-subnav li:first-child');
         const facts = document.querySelector('#show-right-subnav li:last-child');
+        const overviewSection = document.getElementById('show-details-overview');
+        const showDetailsArea = document.getElementById('show-details-area');
         overview.classList.add('selected');
         facts.classList.remove('selected');
+        overviewSection.scrollIntoView();
+        showDetailsArea.scrollBy({top: -60});
     }
 
     const clickFacts = () => {
         const overview = document.querySelector('#show-right-subnav li:first-child');
         const facts = document.querySelector('#show-right-subnav li:last-child');
+        const factSection = document.getElementById('show-details-facts');
+        const showDetailsArea = document.getElementById('show-details-area');
         overview.classList.remove('selected');
         facts.classList.add('selected');
+        factSection.scrollIntoView();
+        showDetailsArea.scrollBy({top: -60});
     }
 
     return(
