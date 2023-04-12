42c0ce98039d1c41d5fc6285fdb4ef7808c46e05
Steven Paalz <stevenpaalz@gmail.com>
2023-04-12 14:07:08 -0400

    update main

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,30 +1,33 @@
 import "./SearchBar.css";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 function SearchBar() {
     const history = useHistory();
     const sessionUser = useSelector(state => state.session.user);
+    const [searchField, setSearchField] = useState("")
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        debugger
     }
 
     return(
         <div id="index-page-search-bar" className="open-sans">
             <div id="search-bar-form">
                 <form onSubmit={handleSubmit} className="open-sans" id='index-search'>
-                    <input id='search-input-area' placeholder='City, Neighborhood, ZIP, Address'></input>
+                    <input id='search-input-area' onChange={e=>setSearchField(e.target.value)}value={searchField} placeholder='City, Neighborhood, ZIP, Address'></input>
                     <span><button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button></span>
                 </form>
-                <button id='sale-type-sort' className='search-bar-sort'>
+                {/* <button id='sale-type-sort' className='search-bar-sort'>
                     <form>
                         <label htmlFor="for-sale-radio">For Sale</label>
                         <input id='for-sale-radio' type='radio' />
                         <label htmlFor="for-rent-radio">Rent</label>
                         <input id='for-rent-radio' type='radio' />
                     </form>
-                </button>
+                </button> */}
             </div>
             <div id="saved-homes">
                 <button onClick={()=>history.replace(`/${sessionUser.id}/favorites`)}>Saved Homes</button>
