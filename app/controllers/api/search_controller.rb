66f1e870a4bec5078c1a4757a2210eac88f386d6
Steven Paalz <stevenpaalz@gmail.com>
2023-05-03 10:46:43 -0400

    enable live search for partial queries

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -4,11 +4,11 @@ class Api::SearchController < ApplicationController
         query_string = params[:q]
         type = params[:type]
         if query_string && type
-            @listings = Listing.where('sale_type = ?', type).and(Listing.where('zipcode = ?', query_string).or(Listing.where('city = ?', query_string)).or(Listing.where('state = ?', query_string)).or(Listing.where('street_address = ?', query_string)))
+            @listings = Listing.where('sale_type = ?', type).and(Listing.where('zipcode LIKE ?', "#{query_string}%").or(Listing.where('city LIKE ?', "#{query_string}%")).or(Listing.where('state = ?', query_string)).or(Listing.where('street_address LIKE ?', "#{query_string}%")))
         elsif (!query_string || query_string === "") && type
             @listings = Listing.where('sale_type = ?', type)
         elsif (!type || type === "") && query_string
-            @listings = Listing.where('zipcode = ?', query_string).or(Listing.where('city = ?', query_string)).or(Listing.where('state = ?', query_string)).or(Listing.where('street_address = ?', query_string))
+            @listings = Listing.where('zipcode LIKE ?', "#{query_string}%").or(Listing.where('city LIKE ?', "#{query_string}%")).or(Listing.where('state = ?', query_string)).or(Listing.where('street_address LIKE ?', "#{query_string}%"))
         else
             @listings = Listing.all
         end
